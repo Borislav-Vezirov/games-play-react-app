c41b2ce6569598fb4ce492bfdf5c580d39895096
Borislav Vezirov <bobi.vez@gmail.com>
2022-04-04 17:52:11 +0300

    extract fetch functionality in services folder

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,29 +1,32 @@
 import { useEffect, useState } from 'react';
 import GameCard from './GameCard.js';
+import * as GameService from '../services/gameService.js'
 
 
 function Catalog(){
 
     const [games, setGames] = useState([]);
+    const [loading, setloading] = useState(false);
 
     useEffect(() => {
-
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
+        setloading(true);
+        GameService.getAllGames()
             .then(result => {
-                console.log(result);
+                setloading(false)
                 setGames(result);
             });
+
     }, []);
-
-
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-
-            { games.map(x => <GameCard game={x} />) }
-            <h3 className="no-articles">No articles yet</h3>
+            {loading 
+                ?  <h3 className="no-articles">Loading...</h3> 
+                :  games.length > 0 ? 
+                   games.map(x => <GameCard  key={x._id} game={x} />) 
+                   : <h3 className="no-articles">No articles yet</h3>
+            }      
         </section>
     )
 }
